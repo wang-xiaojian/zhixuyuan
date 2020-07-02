85e0c08e2ac33e61660d6a52eef1b5985f4b8c33
王金宝 <sinian900902@126.com>
2020-07-02 11:26:56 +0800

    初始化项目

---
+++ src/main/java/cn/zhixuyuan/zhixuyuan/ZhixuyuanApplication.java
@@ -0,0 +1,13 @@
+package cn.zhixuyuan.zhixuyuan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZhixuyuanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZhixuyuanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/zhixuyuan/zhixuyuan/ZhixuyuanApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.zhixuyuan.zhixuyuan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZhixuyuanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
